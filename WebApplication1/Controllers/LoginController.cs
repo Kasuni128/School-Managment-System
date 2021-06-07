b3a5cac6833392b4a63145b1bdb30776aa1677eb
kasuni128 <kasuniwaththage128@gmail.com>
2021-06-07 22:55:20 +0530

    Updated Files

---
+++ WebApplication1/Controllers/LoginController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class LoginController : Controller
+    {
+       
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(user objchk)
+        {
+            if (ModelState.IsValid)
+            {
+                using(schoolEntities db = new schoolEntities())
+                {
+                    var obj = db.users.Where(a => a.username.Equals(objchk.username) && a.password.Equals(objchk.password)).FirstOrDefault();
+
+                    if (obj != null)
+                    {
+                        Session["UserID"] = obj.id.ToString();
+                        Session["UserName"] = obj.username.ToString();
+                        return RedirectToAction("Index", "Home");
+                    }
+
+
+                    else
+                    {
+                        ModelState.AddModelError("", "The Username or Password Incorrect");
+                    }
+                }
+            }
+           
+            return View(objchk);
+        }
+
+       
+    }
+}
